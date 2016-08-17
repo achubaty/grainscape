c67557e4e44218e52e030b3702eb642a8c154be3
Alex Chubaty <alex.chubaty@gmail.com>
2016-08-17 14:09:49 -0600

    remove fpthresh option

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,7 @@
 #' @importFrom methods loadMethod
 .onLoad <- function(libname, pkgname) {
-  options(gs.fpthresh = 1e-4)
 }
 
 .onUnload <- function(libpath) {
-  options(gs.fpthresh = NULL)
   library.dynam.unload("grainscape2", libpath)
 }
