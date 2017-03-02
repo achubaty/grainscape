ae7bfbebe4c669c8ca5804c4cea43cb7bc6276e9
Alex Chubaty <alex.chubaty@gmail.com>
2017-03-02 10:54:20 -0700

    add test cases for NA rasters (#28, #32)
    @samdoctolero the test passes, which suggests the spatial graph is being correctly built, but the raster is incorrect or being plotted incorrectly.

--- tests/testthat/test-gsMPG.R
+++ tests/testthat/test-gsMPG.R
@@ -1,4 +1,4 @@
-test_that("MPG handles NA vaules correctly", {
+test_that("MPG handles NA values correctly (#28)", {
   library(igraph)
   library(raster)
 
@@ -26,7 +26,7 @@ test_that("MPG handles NA vaules correctly", {
   ids_l <- which(mpg@lcpLinkId[] < 0)
   expect_false(any(ids_l %in% NAs))
 
-  ## more spohisticatied case
+  ## more sophisticatied case
   tinyNA <- raster(system.file("extdata/tiny.asc", package = "grainscape"))
   tinyNA[1:95, 46:55] <- NA_integer_
 
@@ -43,4 +43,61 @@ test_that("MPG handles NA vaules correctly", {
   ## no links in the NA region
   ids_l <- which(tinyPatchMPG@lcpLinkId[] < 0)
   expect_false(any(ids_l %in% tinyNAs))
+
+  ## even more complex map with NA regions
+  naError <- raster(system.file("extdata/naErrorExample.asc", package = "grainscape"))
+  naErrorCost <- reclassify(naError, rcl = cbind(c(1, 2, 3, 4), c(1, 5, 10, 12)))
+  naErrorMPG <- MPG(cost = naErrorCost, patch = (naErrorCost == 1))
+
+  naErrorNAs <- which(is.na(naError[]))
+
+  ## no voronoi spread in the NA region
+  ids_v <- which(naErrorMPG@voronoi[] > 0)
+  expect_false(any(ids_v %in% naErrorNAs))
+
+  ## no links in the NA region
+  ids_l <- which(naErrorMPG@lcpLinkId[] < 0)
+  expect_false(any(ids_l %in% naErrorNAs))
+})
+
+test_that("MPG contains links to all patches (#32)", {
+  library(igraph)
+  library(raster)
+
+  ## simple map with no NA regions
+  tiny <- raster(system.file("extdata/tiny.asc", package = "grainscape"))
+  tinyCost <- reclassify(tiny, rcl = cbind(c(1, 2, 3, 4), c(1, 5, 10, 12)))
+  tinyMPG <- MPG(cost = tinyCost, patch = (tinyCost == 1))
+
+  g <- tinyMPG$mpg
+  #g <- simplify(g, remove.multiple = TRUE, remove.loops = TRUE)
+  expect_equal(count_components(g), 1)
+
+  ## simple map with NA regions
+  tinyNA <- raster(system.file("extdata/tiny.asc", package = "grainscape"))
+  tinyNA[1:95, 46:55] <- NA_integer_
+  tinyNACost <- reclassify(tinyNA, rcl = cbind(c(1, 2, 3, 4), c(1, 5, 10, 12)))
+  tinyNAMPG <- MPG(cost = tinyNACost, patch = (tinyNACost == 1))
+
+  g <- tinyNAMPG$mpg
+  #g <- simplify(g, remove.multiple = TRUE, remove.loops = TRUE)
+  expect_equal(count_components(g), 1)
+
+  ## simple map with non-connected subgraphs expected
+  tinyNA <- raster(system.file("extdata/tiny.asc", package = "grainscape"))
+  tinyNA[1:100, 49:50] <- NA_integer_
+  tinyNACost <- reclassify(tinyNA, rcl = cbind(c(1, 2, 3, 4), c(1, 5, 10, 12)))
+  tinyNAMPG <- MPG(cost = tinyNACost, patch = (tinyNACost == 1))
+
+  g <- tinyNAMPG$mpg
+  #g <- simplify(g, remove.multiple = TRUE, remove.loops = TRUE)
+  expect_equal(count_components(g), 2)
+
+  ## more complex map with NA regions
+  naError <- raster(system.file("extdata/naErrorExample.asc", package = "grainscape"))
+  naErrorMPG <- MPG(cost = naError, patch = (naError == 1))
+
+  g <- naErrorMPG$mpg
+  #g <- simplify(g, remove.multiple = TRUE, remove.loops = TRUE)
+  expect_equal(count_components(g), 1)
 })
