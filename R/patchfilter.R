1ad90978ee752825d95babb3aaa8105b23c42f27
ecologics <pgalpern@ucalgary.ca>
2017-07-13 21:48:58 -0600

    Create a patchfilter() function (close #40)
    This function fulfills specifications listed in this issue.  R CMD check has not been done on this commit.

---
+++ R/patchfilter.R
@@ -0,0 +1,115 @@
+#' Filter out patches smaller than a specified area
+#'
+#' @description
+#' This function is to pre-process patch rasters prior to their
+#' use with \code{\link{MPG}}.
+#'
+#' It examines a binary raster to identify all
+#' patches or clumps of cells with values \code{=1}, determines their area, and
+#' returns a binary raster where only patches of area greater than or equal to
+#' the specified amount are represented.
+#'
+#' This is helpful when analyzing habitat connectivity models
+#' where patches represent a land cover or habitat type.  For example,
+#' a raster may have patches of a certain habitat type of insufficient
+#' area to support the ecological process of interest.  Another use case is remote
+#' sensing classification errors that have introduced artifacts.  Filtering can
+#' help in both cases.
+#'
+#'
+#' @param x             A binary raster (i.e. consisting of \code{0}, \code{1},
+#'                      or \code{NA} cells), where cells \code{=1} represent
+#'                      patches
+#'
+#' @param cells         The minimum number of cells that constitute a patch.
+#'                      Default \code{NULL}. Only one of \code{cells} or
+#'                      \code{area} may be specified.
+#'
+#' @param area          The minimum area that constitutes a patch (where area is
+#'                      calculated in the coordinate reference system of the
+#'                      raster by multiplying the count of cells in patch by the x
+#'                      and y resolution of a raster cell).  Default \code{NULL}.
+#'                      Only one of \code{cells} or \code{area} may be specified.
+#'
+#' @param ...           Additional arguments passed to the \code{\link{clump}}
+#'                      function in the \code{raster} package.  For example
+#'                      \code{directions = 4} may be used to be more conservative
+#'                      about which cells constitute a patch.
+#'
+#' @return   A binary raster where all patches (i.e. clumped areas \code{=1})
+#' are greater than the specified area.
+#'
+#' @author Paul Galpern and Alex Chubaty
+#' @docType methods
+#' @export
+#' @importFrom raster clump res
+#'
+#' @include classes.R
+#' @rdname patchfilter
+#' @seealso \code{\link{MPG}}
+#' @examples
+#' \dontrun{
+#' library(raster)
+#'
+#' ## Load raster landscape
+#' tiny <- raster(system.file("extdata/tiny.asc", package = "grainscape"))
+#'
+#' ## Create a resistance surface from a raster using an is-becomes reclassification
+#' tinyCost <- reclassify(tiny, rcl = cbind(c(1, 2, 3, 4), c(1, 5, 10, 12)))
+#'
+#' ## Produce a patch-based MPG where patches are resistance features = 10
+#' ## and all patches are greater than or equal to 2 cells in size.
+#' tinyPatchMPG <- MPG(cost = tinyCost, patch = patchfilter(tinyCost == 10, cells = 2))
+#' plot(tinyPatchMPG)
+#'
+#' ## Compare to removal of patches greater than or equal to 40 cells in size!
+#' tinyPatchMPG <- MPG(cost = tinyCost, patch = patchfilter(tinyCost == 10, cells = 40))
+#' plot(tinyPatchMPG)
+#'
+#' ## Use a rook/castle 4-direction case rather than the queen 8-direction case
+#' ## to identify neighbouring cells in a patch
+#' tinyPatchMPG <- MPG(cost = tinyCost,
+#'    patch = patchfilter(tinyCost == 10, cells = 40, directions = 4))
+#' plot(tinyPatchMPG)
+#' }
+#'
+setGeneric(
+  "patchfilter",
+  function(x, cells = NULL, area = NULL, ...) {
+    standardGeneric("patchfilter")
+})
+
+#' @export
+#' @rdname export
+setMethod(
+  "patchfilter",
+  signature = "RasterLayer",
+  definition = function(x, cells = NULL, area = NULL, ...) {
+
+  if (!xor(is.null(cells), is.null(area))) {
+    stop("either the cells or area parameter must be specified")
+  }
+
+  if (any(!(unique(x[]) %in% c(NA, 0, 1)))) {
+    stop("x must be a binary raster containing 0, 1 or NA values")
+  }
+
+  if (!is.null(area)) {
+    cellThresh <- ceiling(area / (res(x)[1] * res(x)[2]))
+  }
+  else {
+    cellThresh <- cells
+  }
+
+  patch <- clump(x, ...)
+  fp <- freq(patch)
+  rmpatch <- fp[fp[,2] < cellThresh, 1]
+
+  out <- patch
+  out[patch[] %in% rmpatch] <- NA
+  out <- !is.na(out)
+
+  return(out)
+
+})
+
