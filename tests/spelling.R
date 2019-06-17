4143a316cec54f26296e49b8da07c9084b79e85d
Alex Chubaty <alex.chubaty@gmail.com>
2019-06-17 15:58:10 -0600

    don't test spelling on travis
    appveyor seems to be fine

--- tests/spelling.R
+++ tests/spelling.R
@@ -1,2 +1,2 @@
-if (!isTRUE(Sys.getenv("CI")) && requireNamespace("spelling", quietly = TRUE))
+if ((tolower(Sys.getenv("TRAVIS")) != "true") && requireNamespace("spelling", quietly = TRUE))
   spelling::spell_check_test(vignettes = TRUE, error = FALSE, skip_on_cran = TRUE)
